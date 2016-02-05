811b8c28433a6dc0215914a769517e9775c6fe05
Kyle Kelley <rgbkrk@gmail.com>
2016-02-04 21:23:51 -0600

    Problem: findAll's test wasn't very robust
    Solution: Check more properties

--- test/traverse_spec.js
+++ test/traverse_spec.js
@@ -4,8 +4,19 @@ const expect = require('chai').expect;
 
 describe('findAll', () => {
   it('retrieves a collection of kernel specs', () => {
-    return findAll().then(dirs => {
-      expect(dirs).to.have.any.keys('python3', 'python2');
+    return findAll().then(kernelspecs => {
+      expect(kernelspecs).to.have.any.keys('python3', 'python2');
+
+      const defaultKernel = kernelspecs.python2 || kernelspecs.python3;
+
+      expect(defaultKernel).to.have.property('spec');
+      expect(defaultKernel).to.have.property('resources_dir');
+
+      const spec = defaultKernel.spec;
+
+      expect(spec).to.have.property('display_name');
+      expect(spec).to.have.property('argv');
+
     });
   });
 });
